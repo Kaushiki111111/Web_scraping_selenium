074f24951f5105a25aac38a0ec1c4ac56bc8c798
KaushikiDwivedi <98608496+Kaushiki111111@users.noreply.github.com>
2022-06-02 10:14:18 +0000

    Youtube trending

--- scraper.py
+++ scraper.py
@@ -1,1 +1,9 @@
-print("Hello")
+import requests
+Youtube_trending_url="https://www.youtube.com/feed/trending"
+r=requests.get(Youtube_trending_url)
+print('Status_code',r.status_code)
+print('if returns 200->successful',r.status_code==200)
+print(r.text[:5000])
+
+
+
