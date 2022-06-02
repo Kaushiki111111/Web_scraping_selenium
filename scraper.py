66a3bff91b428bd8c881068d9013db67a60b22b1
KaushikiDwivedi <98608496+Kaushiki111111@users.noreply.github.com>
2022-06-02 11:48:39 +0000

    added page title

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 Youtube_trending_url="https://www.youtube.com/feed/trending"
+#Requests does not execute java script
 r=requests.get(Youtube_trending_url)
 print('Status_code',r.status_code)
 print('if returns 200->successful',r.status_code==200)
@@ -7,4 +9,10 @@ print('if returns 200->successful',r.status_code==200)
 with open('trending.html','w')as f:
   f.write(r.text)
 
-
+doc=BeautifulSoup(r.text,'html.parser')
+print('page-title',doc.title)
+print('Page Title text',doc.title.text)
+#To find video divs:
+video_divs=doc.find_all('div',class_=" ytd-video-renderer")
+print(f'Found {(len(video_divs))} videos')
+   
