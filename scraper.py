7da7911b6c31610e1cf7aad821b9a6dc22661121
KaushikiDwivedi <98608496+Kaushiki111111@users.noreply.github.com>
2022-06-02 10:17:20 +0000

    read as html

--- scraper.py
+++ scraper.py
@@ -3,7 +3,8 @@ Youtube_trending_url="https://www.youtube.com/feed/trending"
 r=requests.get(Youtube_trending_url)
 print('Status_code',r.status_code)
 print('if returns 200->successful',r.status_code==200)
-print(r.text[:5000])
-
+#print(r.text[:5000])
+with open('trending.html','w')as f:
+  f.write(r.text)
 
 
