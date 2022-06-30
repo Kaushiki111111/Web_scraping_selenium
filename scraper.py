136137eeb6da3ec5d6293506b549ee2266718e83
KaushikiDwivedi <98608496+Kaushiki111111@users.noreply.github.com>
2022-06-30 07:01:40 +0000

    title, url, thumbnail_url added

--- scraper.py
+++ scraper.py
@@ -17,6 +17,11 @@ def get_videos(driver):
   video_divs="ytd-video-renderer"
   VIDEO_DIV=driver.find_elements(by=By.TAG_NAME,value=video_divs)
   return VIDEO_DIV
+print("Parsing the first video")
+#title,url,thumbnail_url,channel,views,uploaded,description
+
+
+  
 
 
 if __name__=="__main__":
@@ -25,5 +30,17 @@ if __name__=="__main__":
   print("Fetching the page:")
   videos=get_videos(driver)
   print(f"Found {(len(videos))}videos in trending")
+  print("Parsing the first video")
+  #title,url,thumbnail_url,views,uploaded,description
+
+  video=videos[0]
+  title_tag=video.find_element(By.ID,"video-title")
+  title=title_tag.text
+  url=title_tag.get_attribute('href')
+  thumbnail_url=video.find_element(By.TAG_NAME,"img")
+  thumbnail_tag=thumbnail_url.get_attribute("src")
   
- 
+  print("Title:",title)
+  print("Url:",url)
+  
+  print("Thumbnail_url:",thumbnail_tag)
