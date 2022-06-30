e7eb37894746457b7f4b0a02e39ace14f16fcefd
KaushikiDwivedi <98608496+Kaushiki111111@users.noreply.github.com>
2022-06-30 09:39:38 +0000

    Dataframe created consisting of 10 rows and 6 columns

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 youtube_trending_url="https://www.youtube.com/feed/trending"
-
+import pandas as pd
 def get_driver():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
@@ -15,33 +15,46 @@ def get_videos(driver):
   driver.get(youtube_trending_url)
   print("Fetching the divs:")
   video_divs="ytd-video-renderer"
-  VIDEO_DIV=driver.find_elements(by=By.TAG_NAME,value=video_divs)
-  return VIDEO_DIV
-print("Parsing the first video")
+  videos=driver.find_elements(By.TAG_NAME,video_divs)
+  return videos
+#print("Parsing the first video")
 #title,url,thumbnail_url,channel,views,uploaded,description
 
 
-  
-
-
-if __name__=="__main__":
-  print("Creating driver")
-  driver=get_driver()
-  print("Fetching the page:")
-  videos=get_videos(driver)
-  print(f"Found {(len(videos))}videos in trending")
-  print("Parsing the first video")
-  #title,url,thumbnail_url,views,uploaded,description
-
-  video=videos[0]
+def parse_videos(video):
   title_tag=video.find_element(By.ID,"video-title")
   title=title_tag.text
   url=title_tag.get_attribute('href')
   thumbnail_url=video.find_element(By.TAG_NAME,"img")
   thumbnail_tag=thumbnail_url.get_attribute("src")
+  channel_name=video.find_element(By.ID,"container")
+  channel=channel_name.text
+  views=video.find_element(By.ID,"metadata-line").text
+  desc=video.find_element(By.ID,"description-text").text
   
-  print("Title:",title)
-  print("Url:",url)
+  return{
+    "Title:":title,
+    "Url:":url,
+    "Thumbnail_url:":thumbnail_tag,
+    "Channel name:":channel,
+    "Views:":views,
+    "Description of the video":desc
+    }
   
-  print("Thumbnail_url:",thumbnail_tag)
+if __name__=="__main__":
+  print("Creating driver")
+  driver=get_driver()
+  print("Fetching the trending videos:")
+  videos=get_videos(driver)
+  print(f"Found {(len(videos))}videos in trending")
+  #print("Parsing the first video")
+  #title,url,thumbnail_url,views,uploaded,description
+  videos_data=[parse_videos(video) for video in videos[:10]]
   
+  videos_df=pd.DataFrame(videos_data)
+  print( videos_df)
+  videos_df.to_csv("videos_trending.csv",index=None)
+  #print(videos_data)
+  #print(videos_data[3])
+  #print(f"Save to a csv file")
+ 
